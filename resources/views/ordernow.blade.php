41d033f87e8c79e6e40189c46f5a928afd8cf398
nitan <jananitan@gmail.com>
2022-08-18 11:26:17 +0530

    Payment page  created

--- resources/views/ordernow.blade.php
+++ resources/views/ordernow.blade.php
@@ -1,9 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="custom-product">
-        <div class="col-md-4">
-            
+    <div class="container custom-product">
+        <div class="container col-md-4">
+            <table class="table">
+                
+                <tbody>
+                  <tr>
+                    <th colspan="4">Amount</td>
+                    <td>$ {{$total}}</td>
+                  </tr>
+                  <tr>
+                    <th colspan="4">Tax</td>
+                    <td>$ 0</td>
+                  </tr>
+                  <tr>
+                    <th colspan="4">Delivery</td>
+                    <td>$ 10</td>
+                  </tr>
+                  <tr>
+                    <th colspan="4">Total Amount</td>
+                    <td>$ {{$total +10}}</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <form>
+                <div class="mb-3">
+                  <textarea type="email" class="form-control" placeholder="Enter your address" aria-describedby="emailHelp"></textarea>
+                </div>
+                <div class="mb-3">
+                  <label for="pwd" class="form-label"><b>Payment Method</b></label><br><br>
+                  <input type="radio" name= "payment"><span>Online Payment</span><br><br>
+                  <input type="radio" name= "payment"><span>EMI Payment</span><br><br>
+                  <input type="radio" name= "payment"><span>Payment on Delivery</span><br><br>
+                </div>
+                
+                <button type="submit" class="btn btn-success">Order Now</button>
+              </form>
         </div>
     </div>
 @endsection
