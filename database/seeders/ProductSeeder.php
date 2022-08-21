c646ef105f3085b274bb6e9f5e9be7725c7a2d83
nitan <jananitan@gmail.com>
2022-08-21 16:28:38 +0530

    fixing sum error in orderNow function in ProductController

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,21 +19,21 @@ class ProductSeeder extends Seeder
         DB::table('products')->insert([
             [
                 'name' => 'Panasonic tv',
-                'price' => '1000',
+                'price' => 1000,
                 'description' => 'Samsung TV with a lot of features',
                 'category' => 'tv',
                 'gallery' => 'https://www.lg.com/us/images/cell-phones/md07522101/gallery/desktop-01.jpg'
             ],
             [
                 'name' => 'Sony tv',
-                'price' => '1200',
+                'price' => 1200,
                 'description' => 'Sony TV with a lot of features',
                 'category' => 'mobile',
                 'gallery' => 'https://fdn2.gsmarena.com/vv/bigpic/oppo-reno8.jpg'
             ],
             [
                 'name' => 'Samsung Fridge',
-                'price' => '2200',
+                'price' => 2200,
                 'description' => 'Samsung fridge with a lot of features',
                 'category' => 'mobile',
                 'gallery' => 'https://image.shutterstock.com/image-vector/realistic-smartphone-mockup-cellphone-frame-600w-1401850631.jpg'
